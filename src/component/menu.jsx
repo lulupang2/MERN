2c9aab7ac415b4fffa98ac8837831e0644566d2e
KENKEN0803 <blackflamekr@gmail.com>
2021-08-29 19:40:10 +0900

    로그인...

--- src/component/menu.jsx
+++ src/component/menu.jsx
@@ -1,6 +1,12 @@
 import '../res/menu.css';
 import { NavLink } from 'react-router-dom';
+import GoogleLogin from 'react-google-login';
+
 export const Menu = (props) => {
+    const responseGoogle = (response) => {
+        console.log(response);
+    }
+
     return (
         <div className="navmenu">
             <div className="temp">뭔가 들어갈듯</div>
@@ -11,6 +17,13 @@ export const Menu = (props) => {
                     <li>메뉴</li>
                     <li><NavLink className="nav-link" to="/board">게시판</NavLink></li>
                     <li><NavLink className="nav-link" to="/eat">뭐먹</NavLink></li>
+                    <li><GoogleLogin
+                        clientId="1047644500942-67e2i9lva85spte4rt32sr7vjj0ah5lf.apps.googleusercontent.com"
+                        buttonText="Login"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
+                        cookiePolicy={'single_host_origin'}
+                    />,</li>
                 </ul>
             </div>
         </div>
